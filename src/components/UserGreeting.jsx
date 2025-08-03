416d74d761651cb9ff794a6c0003d991e47749fc
Marlene <marleneoteiza@outlook.com>
2025-08-03 18:49:50 -0300

    Resolved merge conflicts

--- src/components/UserGreeting.jsx
+++ src/components/UserGreeting.jsx
@@ -1,22 +1,22 @@
 
 
-const UserGreeting = () => {
-    return (
-        <div style={styles.div}>
-            <h1 style={styles.titulo}>Hola Marlene, ¡bienvenida a tu tienda!</h1>
-        </div>
-    )
-}
+// const UserGreeting = () => {
+//     return (
+//         <div style={styles.div}>
+//             <h1 style={styles.titulo}>Hola Marlene, ¡bienvenida a tu tienda!</h1>
+//         </div>
+//     )
+// }
 
-const styles = {
-    div: {
-        padding: '10rem',
-        backgroundColor: '#e0f7fa',
-    },
-    titulo: {
-        color: '#333',
-        textAlign: 'center',
-    }
-};
+// const styles = {
+//     div: {
+//         padding: '10rem',
+//         backgroundColor: '#e0f7fa',
+//     },
+//     titulo: {
+//         color: '#333',
+//         textAlign: 'center',
+//     }
+// };
 
-export default UserGreeting;
+// export default UserGreeting;
